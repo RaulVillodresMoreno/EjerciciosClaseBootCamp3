ae3d339b226e2a782808e5241fb877c8f405fab8
RaulVillodresMoreno <villodres.raulmoreno@gmail.com>
2022-10-26 10:12:11 +0200

    Primer ejercicio

---
+++ saludo-idioma/saludo-idioma/src/main/java/com/nttdata/bootcamp/saludoidioma/domain/Saludo.java
@@ -0,0 +1,39 @@
+package com.nttdata.bootcamp.saludoidioma.domain;
+
+public class Saludo {
+	private String idioma;
+	
+	public Saludo(String idioma) {
+		this.idioma = idioma;
+	}
+	
+	public String mensajeSaludo() {
+		String mensaje = "";
+		switch(idioma) {
+		case "Castellano":
+			mensaje = "Bienvenido";
+			break;
+		case "Inglés":
+			mensaje = "Welcome";
+			break;
+		case "Italiano":
+			mensaje = "Benvenuto";
+			break;
+		default:
+			mensaje = "Bienvenido";
+			break;
+		}
+		return mensaje;
+	}
+
+	public String getIdioma() {
+		return idioma;
+	}
+
+	public void setIdioma(String idioma) {
+		this.idioma = idioma;
+	}
+	
+	
+		
+}
